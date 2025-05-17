31e0775043f1c5286f092fb311160ca9f8c035f5
John <42081256+JohnV50@users.noreply.github.com>
2025-05-17 17:27:00 -0400

    updated a function

--- src/modules/commands/unblacklist.py
+++ src/modules/commands/unblacklist.py
@@ -1,13 +1,13 @@
 from discord.ext import commands
 from resources.mrcookie import instance as bot
-from resources.checks import is_admin, validate_userid, is_blacklisted
+from resources.checks import is_admin, validate_user, is_blacklisted
 
 @bot.command()
 async def unblacklist(ctx, temp_ID):
     try:
         if await is_admin(ctx.author.id) == False: raise Exception("You don't have permission to run this command.")
         
-        userID = await validate_userid(temp_ID) 
+        userID = await validate_user(temp_ID) 
         if userID == None: raise Exception("Invalid user.")
                 
         if await is_blacklisted(userID) == False: raise Exception("User is not blacklisted.")
