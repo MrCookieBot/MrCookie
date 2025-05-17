542633c179a995d40f85b0b1ae7993a8a8779a00
John <42081256+JohnV50@users.noreply.github.com>
2025-05-17 17:27:22 -0400

    improved a check

--- src/resources/checks.py
+++ src/resources/checks.py
@@ -20,26 +20,22 @@ async def is_blacklisted(userID):
         return False
         
 
-async def validate_userid(userID):
-    temp_id = userID.strip("<@!>")
-    if temp_id.isdigit():
-        new_id = int(temp_id)
-        if len(str(new_id)) > 17:
-            return new_id
-        else:
-            return None
-    else:
+async def validate_user(userID, guild):
+    userID = userID.strip("<@!>")
+    if userID == "0" or userID.isdigit() == False or len(userID) < 17:
         return None
+    else:
+        return userID
     
 
+# user data
 async def lookup_database(userID, guildID):
     data = await bot.db.find_user({"_id": str(guildID), f"users.{userID}": {"$exists": True}})
     if data != None:
         return data
     else:
         return False
-
-
+    
 async def new_database(userID, guildID):
     newUser = {"Cookies": 0, "Streaks": 0, "DailyExpire": datetime.now() - timedelta(hours = 24), "RobExpire": datetime.now() - timedelta(hours = 24), "DailyMultiplier": 0, "RobChances": 7, "DailyMultExpire": datetime.now(), "RobExpire": datetime.now(), "Inventory": "Empty", "RobProtection": 0}
     await bot.db.update_user({"_id": str(guildID)}, {'$set': {"users." + str(userID) : {**newUser}}})
@@ -51,3 +47,18 @@ async def update_database(userID, guildID, updated_dict):
 
 async def update_value(userID, guildID, item, new_value):
     await bot.db.update_user({"_id": str(guildID)}, {'$set': {"users." + str(userID) + "." + item: new_value}})
+
+# counter data
+async def lookup_counter(guildID):
+    data = await bot.db.find_user({"_id": str(guildID), f"settings.{"counter"}": {"$exists": True}})
+    if data == None:
+        return False
+    else:
+        return data
+
+async def new_counter(guildID):
+    newGuild = {"Channel": 0, "Counter": 0, "lastUser": 0}
+    await bot.db.update_user({"_id": str(guildID)}, {'$set': {"settings." + "counter" : {**newGuild}}})
+
+async def update_counter(guildID, item, new_value):
+    await bot.db.update_user({"_id": str(guildID)}, {'$set': {"settings." + "counter" + "." + item: new_value}})
