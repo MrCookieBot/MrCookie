96aed9a853f0262518e11959f1b41ddcca210032
Nub <38899321+One-Nub@users.noreply.github.com>
2025-05-22 20:26:24 +0000

    Add message when bot is ready

--- src/modules/events/on_ready.py
+++ src/modules/events/on_ready.py
@@ -1,11 +1,14 @@
-from resources.mrcookie import instance as bot
+import logging
+
 from discord import CustomActivity, Status
+
+from resources.mrcookie import instance as bot
 
 
 @bot.event
 async def on_ready():
     await bot.change_presence(
-        activity = CustomActivity(name = "Baking cookies!"),
-        status= Status.online,
+        activity=CustomActivity(name="Baking cookies!"),
+        status=Status.online,
     )
-
+    logging.info(f"{bot.user.name}#{bot.user.discriminator} is now logged in & ready!")
