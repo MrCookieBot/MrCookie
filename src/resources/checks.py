a1c77db0dd2e2643d60b18ba9b8a6f2f534572b0
Nub <38899321+One-Nub@users.noreply.github.com>
2025-05-23 03:14:20 +0000

    Format this because I ain't scrolling that far

--- src/resources/checks.py
+++ src/resources/checks.py
@@ -1,9 +1,10 @@
+from datetime import datetime, timedelta
+
 import discord
 
-from resources.mrcookie import instance as bot
 from resources.constants import ADMIN_USERS
+from resources.mrcookie import instance as bot
 
-from datetime import datetime, timedelta
 
 async def is_admin(userID):
     for value in ADMIN_USERS:
@@ -11,14 +12,14 @@ async def is_admin(userID):
             return True
         else:
             return False
-        
+
 
 async def is_blacklisted(userID):
     if await bot.db.find_blacklist({"_id": str(userID)}) != None:
         return True
     else:
         return False
-        
+
 
 async def validate_user(userID, guild):
     userID = userID.strip("<@!>")
@@ -26,7 +27,7 @@ async def validate_user(userID, guild):
         return None
     else:
         return userID
-    
+
 
 # user data
 async def lookup_database(userID, guildID):
@@ -35,18 +36,33 @@ async def lookup_database(userID, guildID):
         return data
     else:
         return False
-    
+
+
 async def new_database(userID, guildID):
-    newUser = {"Cookies": 0, "Streaks": 0, "DailyExpire": datetime.now() - timedelta(hours = 24), "RobExpire": datetime.now() - timedelta(hours = 24), "DailyMultiplier": 0, "RobChances": 7, "DailyMultExpire": datetime.now(), "RobExpire": datetime.now(), "Inventory": "Empty", "RobProtection": 0}
-    await bot.db.update_user({"_id": str(guildID)}, {'$set': {"users." + str(userID) : {**newUser}}})
+    newUser = {
+        "Cookies": 0,
+        "Streaks": 0,
+        "DailyExpire": datetime.now() - timedelta(hours=24),
+        "RobExpire": datetime.now() - timedelta(hours=24),
+        "DailyMultiplier": 0,
+        "RobChances": 7,
+        "DailyMultExpire": datetime.now(),
+        "RobExpire": datetime.now(),
+        "Inventory": "Empty",
+        "RobProtection": 0,
+    }
+    await bot.db.update_user({"_id": str(guildID)}, {"$set": {"users." + str(userID): {**newUser}}})
 
 
 async def update_database(userID, guildID, updated_dict):
-    await bot.db.update_user({"_id": str(guildID)}, {'$set': {"users." + str(userID) : {**updated_dict}}})
+    await bot.db.update_user({"_id": str(guildID)}, {"$set": {"users." + str(userID): {**updated_dict}}})
 
 
 async def update_value(userID, guildID, item, new_value):
-    await bot.db.update_user({"_id": str(guildID)}, {'$set': {"users." + str(userID) + "." + item: new_value}})
+    await bot.db.update_user(
+        {"_id": str(guildID)}, {"$set": {"users." + str(userID) + "." + item: new_value}}
+    )
+
 
 # counter data
 async def lookup_counter(guildID):
@@ -56,9 +72,13 @@ async def lookup_counter(guildID):
     else:
         return data
 
+
 async def new_counter(guildID):
     newGuild = {"Channel": 0, "Counter": 0, "lastUser": 0}
-    await bot.db.update_user({"_id": str(guildID)}, {'$set': {"settings." + "counter" : {**newGuild}}})
+    await bot.db.update_user({"_id": str(guildID)}, {"$set": {"settings." + "counter": {**newGuild}}})
+
 
 async def update_counter(guildID, item, new_value):
-    await bot.db.update_user({"_id": str(guildID)}, {'$set': {"settings." + "counter" + "." + item: new_value}})
+    await bot.db.update_user(
+        {"_id": str(guildID)}, {"$set": {"settings." + "counter" + "." + item: new_value}}
+    )
