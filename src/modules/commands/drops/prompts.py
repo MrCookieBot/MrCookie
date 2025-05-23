060dc3a5af2e8d42139750b01592a23a6c6c163b
Nub <38899321+One-Nub@users.noreply.github.com>
2025-05-23 20:35:58 +0000

    Change example prompts

--- src/modules/commands/drops/prompts.py
+++ src/modules/commands/drops/prompts.py
@@ -1,5 +1,5 @@
 # Format where the key is the question/prompt. The value is the accepted response.
 user_prompts: dict[str, str] = {
-    "question": "answer",
-    "question2": "answer",
+    "Quick! Repeat after me! Say `supercalifragilisticexpialidocious`!": "supercalifragilisticexpialidocious",
+    "What's 9+10?": "21",
 }
